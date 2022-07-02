defc845b27fc443abd82476bbd4b06e17e4f83ce
PhilipK-webdev <philipk179@gmail.com>
2022-07-02 19:45:13 +0300

    edit:change url

--- todo/urls.py
+++ todo/urls.py
@@ -13,11 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django import views
 from django.contrib import admin
 from django.urls import path
 from django.urls import include, re_path
 
 urlpatterns = [
+    path('', views, name="api-overview"),
     path('admin/', admin.site.urls),
     path('api/', include('todoApp.urls')),
 ]
