6e52089a9c76938e3b79f588109df30d369b1435
PhilipK-webdev <philipk179@gmail.com>
2022-07-02 18:51:47 +0300

    edit:configuration

--- todo/settings.py
+++ todo/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import dj_database_url
 import os
 import django_heroku
 from pathlib import Path
@@ -103,7 +104,8 @@ DATABASES = {
         'PORT': '3306',
     }
 }
-
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
